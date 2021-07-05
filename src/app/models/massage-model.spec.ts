5899b71bd75a652682c7711219a778d79a60613c
MarikaSundblad <marika.sundblad89@gmail.com>
2021-07-05 14:35:29 +0200

    småfix

---
+++ src/app/models/massage-model.spec.ts
@@ -0,0 +1,7 @@
+import { MassageModel } from './massage-model';
+
+describe('MassageModel', () => {
+  it('should create an instance', () => {
+    expect(new MassageModel()).toBeTruthy();
+  });
+});
---
+++ src/app/models/massage-model.ts
@@ -0,0 +1,9 @@
+export class MassageModel {
+      constructor(
+            public name?: string,
+            public email?: string,
+            public subject?: string,
+            public massage?: string
+          ) {}
+}
+
